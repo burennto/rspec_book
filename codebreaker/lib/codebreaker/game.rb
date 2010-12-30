1f86c98ccb9a8cab05b8868e54e3173ceb37773f
Brent Chuang <burennto@gmail.com>
2010-12-30 14:24:44 +1100

    Finished chapter 8

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -1,9 +1,18 @@
 module Codebreaker
   class Game
-    def initialize(output)  
+    def initialize(output)
+      @output = output
     end
     
-    def start
+    def start(secret)
+      @secret = secret
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
     end
+    
+    def guess(guess)
+      marker = Marker.new(@secret, guess)
+      @output.puts '+'*marker.exact_match_count + '-'*marker.number_match_count
+    end    
   end
 end
